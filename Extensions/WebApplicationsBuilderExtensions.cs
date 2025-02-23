c0739b2a9e455e21a34287abef3548a425b85015
Carlos Daniel Rivera <dr734659@gmail.com>
2025-02-23 17:50:10 -0500

    feature: init project

---
+++ Extensions/WebApplicationsBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace GatewaySolution.Extensions
+{
+    public static class WebApplicationsBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            var settingsSection = builder.Configuration.GetSection("APISettings");
+
+            var secret = settingsSection.GetValue<string>("Secret");
+            var issuer = settingsSection.GetValue<string>("Issuer");
+            var audience = settingsSection.GetValue<string>("Audience");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            builder.Services.AddAuthentication(auth =>
+                {
+                    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(auth =>
+                {
+                    auth.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = true,
+                        ValidIssuer = issuer,
+                        ValidateAudience = true,
+                        ValidAudience = audience,
+                    };
+                });
+
+            return builder;
+        }
+    }
+}
