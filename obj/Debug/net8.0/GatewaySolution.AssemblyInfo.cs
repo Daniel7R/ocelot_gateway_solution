1aba787a338fa9a84671c1387ebe146fb1346963
Daniel Rivera <daniel.riverar@pragma.com.co>
2025-03-20 08:56:53 -0500

    clean arch

--- obj/Debug/net8.0/GatewaySolution.AssemblyInfo.cs
+++ obj/Debug/net8.0/GatewaySolution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatewaySolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06fb838f2ebc4c1cff37e7fba7ac099ecd10c64c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f9759254daba9bea2fd3a628d3f922e829fe11")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatewaySolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatewaySolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
